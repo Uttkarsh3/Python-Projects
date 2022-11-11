578a69e80737ee98684ca359e196e8bb3ecb8944
Uttkarsh <uttkarshbisht1303@gmail.com>
2022-11-11 19:02:31 +0530

    first commit

---
+++ Madlibs.py
@@ -0,0 +1,5 @@
+adj  = input("Adjective: ")
+verb = input("Verb: ")
+Madlibs = f"Computer programming is so {adj}! It makes me so excited whenever I do code\
+I love to {verb}.Eat Sleep Code Repeat."
+print(Madlibs)
